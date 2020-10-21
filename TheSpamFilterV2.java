31cb799b86fd72dd08c391504500eb774451bd78
Tim Lee <t0966510608@gmail.com>
2020-10-21 21:30:26 +0800

    revert v2

--- TheSpamFilterV2.java
+++ TheSpamFilterV2.java
@@ -37,50 +37,17 @@ public class TheSpamFilterV2 {
                 bigramParser.calculateDocumentProb();
                 String bigramProb = bigramParser.getBigramProb();
                 String bigramCount = bigramParser.getBigramCount();
-//                List<String> countProbPair = Arrays.asList(bigramCount, bigramProb);
+                List<String> countProbPair = Arrays.asList(bigramCount, bigramProb);
                 synchronized (occurrenceMap) {
-                    Integer occurrence = addOccurrence(bigramProb, bigramCount);
-//                    Integer occurrence = occurrenceMap.get(countProbPair);
-//                    if (null == occurrence) occurrence = 0;
-//                    occurrence += 1;
-//                    occurrenceMap.put(countProbPair, occurrence);
+                    Integer occurrence = occurrenceMap.get(countProbPair);
+                    if (null == occurrence) occurrence = 0;
+                    occurrence += 1;
+                    occurrenceMap.put(countProbPair, occurrence);
                     System.out.println(bigramCount + ", " + bigramProb + ", " + occurrence);
                 }
             });
         }
 
         threadPool.shutdown();
-    }
-
-    static Integer addOccurrence(String bigramProb, String bigramCount) {
-        List<String> countProbPair = Arrays.asList(bigramCount, bigramProb);
-        Integer occurrence = occurrenceMap.get(countProbPair);
-        if (null == occurrence) occurrence = 0;
-        occurrence += 1;
-        occurrenceMap.put(countProbPair, occurrence);
-        return occurrence;
-    }
-}
-
-@
-class TheSpamFilterV2Test {
-
-
-    public TheSpamFilterV2Test() {
-
-    }
-
-    @Test
-    public whenAddOccurenceThreeTimesShouldMapHasCorrectValue() {
-        Integer o1 = TheSpamFilterV2.addOccurrence("12345", "1");
-        Integer o2 = TheSpamFilterV2.addOccurrence("1234", "2");
-        Integer o3 = TheSpamFilterV2.addOccurrence("12345", "1");
-
-
-        assertEqual(TheSpamFilterV2.occurrenceMap.get(Arrays.asList("12345", "1")), 2);
-        assertEqual(TheSpamFilterV2.occurrenceMap.get(Arrays.asList("1234", "2")), 1);
-        assertEqual(o1, 1);
-        assertEqual(o2, 1);
-        assertEqual(o3, 2);
     }
 }
